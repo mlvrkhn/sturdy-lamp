8e4905fd555b19717529d0e0d4687f5b4156a05b
mlvrkhn <martin.guide@gmail.com>
2021-06-05 21:10:23 +0200

    After adding key

--- functions/getPrivateRepos.js
+++ functions/getPrivateRepos.js
@@ -1,21 +1,22 @@
 const fetch = require('node-fetch');
 
-const url = 'https://api.github.com/mlvrkhn/repos';
+const url = 'https://api.github.com/user/repos';
 
 exports.handler = async () => {
-    return await fetch(
-        `${url}?visibility=private&affiliation=owner`, {
-            headers: {
-                Accept: 'application/vnd.github.v3+json',
-                Authorization: `token ${process.env.GH_TOKEN}`,
-            }
-        }
-    )
-    .then(resp => resp.json())
-    .then(data => {
-        return {
-            statusCode: 200,
-            headers: {'Content-Type': 'application/json'}
-        }
-    })
+    return await fetch(`${url}?visibility=private&affiliation=owner`, {
+		headers: {
+			Accept: 'application/vnd.github.v3+json',
+			Authorization: `token ${process.env.GH_TOKEN}`,
+		},
+	})
+		.then(resp => {
+            console.log(resp);
+            return resp.json();
+        })
+		.then(data => {
+			return {
+				statusCode: 200,
+				headers: { 'Content-Type': 'application/json' },
+			};
+		});
 }
