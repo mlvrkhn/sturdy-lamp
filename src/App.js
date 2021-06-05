88c95c71cad2f4eaf82e890f84b59a483620cbb1
mlvrkhn <martin.guide@gmail.com>
2021-06-05 21:30:32 +0200

    Up

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with devmentor.pl
+          React with devmentor.pl
         </a>
 
       </header>
